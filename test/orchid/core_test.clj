180e81103e0c26804b068bfe1d4c3f1e2a41f154
Brian Dawn <brian.t.dawn@gmail.com>
2016-02-13 14:34:55 -0600

    Removed test till I have a better method of testing this.

--- test/orchid/core_test.clj
+++ test/orchid/core_test.clj
@@ -2,12 +2,7 @@
   (:require [clojure.test :refer :all]
             [orchid.core :refer :all]))
 
-(defroutes app
-  (GET "/" [] "hello world!"))
 
 
-(grow app 8080)
-(stop-server)
-(grow app 8080 {:dev false})
-(stop-server)
+
 
