142d59bf374d0e17f4ca593b62411396acef7604
Brian Dawn <brian.t.dawn@gmail.com>
2016-04-09 17:04:45 -0500

    Upgraded dependencies.

--- project.clj
+++ project.clj
@@ -9,15 +9,15 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring "1.4.0"]
                  [ring-server "0.4.0"]
-                 [ring/ring-defaults "0.1.5"]
+                 [ring/ring-defaults "0.2.0"]
                  [ring-json-response "0.2.0"]
                  [ring/ring-json "0.4.0"]
-                 [compojure "1.4.0"]
+                 [compojure "1.5.0"]
                  [cheshire "5.5.0"]
-                 [aleph "0.4.1-beta2"]
+                 [aleph "0.4.1"]
 
                  [ring/ring-mock "0.3.0"]
 
                  [potemkin "0.4.3"]
-                 [com.taoensso/timbre "4.2.1"]
+                 [com.taoensso/timbre "4.3.1"]
                  ])
